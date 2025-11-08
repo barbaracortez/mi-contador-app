4cb9183cf7de7c72fd2178f59b308486ff17c9b6
barbaracortez <cortezbarbara.dev@gmail.com>
2025-11-08 20:05:16 -0300

    estilos nuevos y nueva funcionalidad

--- src/app/contador/contador.ts
+++ src/app/contador/contador.ts
@@ -12,21 +12,30 @@ export class ContadorComponent {
   numero = signal(0);
   paso = signal(1);
 
+  min = signal(0);
+  max = signal(100);
+
   aumentar(){
-    this.numero.update(n => n + this.paso())
+    this.numero.update(n =>{
+      const res = n + this.paso();
+      return res > this.max() ? this.max() : res;
+    });
   }
+
   disminuir(){
-    this.numero.update(n => n - this.paso())
+    this.numero.update(n =>{
+      const res = n - this.paso();
+      return res < this.min() ? this.min() : res;
+    
+    });
   }
+
   reiniciar(){
     this.numero.set(0)
   }
 
   actualizarPaso(valor: string){
-    const numeroPaso = Number(valor);
-    if(!isNaN(numeroPaso)) {
-      this.paso.set(numeroPaso)
-    }
+    const n = Number(valor);
+    if(!isNaN(n)) this.paso.set(n)
   }
-
 }
