ac5d31356443f050e0ccc5a37b3b29fd1b7558cf
barbaracortez <cortezbarbara.dev@gmail.com>
2025-11-08 19:06:37 -0300

    Agregando funcionalidad + -

--- src/app/contador/contador.ts
+++ src/app/contador/contador.ts
@@ -9,16 +9,24 @@ import { Component, signal } from '@angular/core';
 
 export class ContadorComponent {
 
-  numero= signal(0);
+  numero = signal(0);
+  paso = signal(1);
 
   aumentar(){
-    this.numero.update(n => n + 1)
+    this.numero.update(n => n + this.paso())
   }
   disminuir(){
-    this.numero.update(n => n - 1)
+    this.numero.update(n => n - this.paso())
   }
   reiniciar(){
     this.numero.set(0)
+  }
+
+  actualizarPaso(valor: string){
+    const numeroPaso = Number(valor);
+    if(!isNaN(numeroPaso)) {
+      this.paso.set(numeroPaso)
+    }
   }
 
 }
